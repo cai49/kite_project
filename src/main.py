8e084fa2c7587983a4c91ab35a6ac41ed9e05f93
CAI_4 <sunsetcity93@gmail.com>
2024-04-27 06:06:25 -0700

    Added content - Added Remove Item button Improved content - Intelligent Add Item behaviour, now adds based on context - Intelligent Remove Item behaviour, now removes based on context

--- src/main.py
+++ src/main.py
@@ -12,8 +12,42 @@ MainWindow.show()
 
 def on_add_button_clicked():
     text_edit = ui.item_textedit.toPlainText()
-    ui.listWidget.addItem(text_edit)
-    print(text_edit)
+
+    indexes = ui.listWidget.selectedItems()
+
+    if len(indexes) > 0:
+        y = 0
+        for i, k in enumerate(indexes):
+            index = ui.listWidget.indexFromItem(k).row()
+            if index > y:
+                y = index
+
+        print(f"Added item {text_edit} at index {y+1}")
+        ui.listWidget.insertItem(y+1, text_edit)
+    else:
+        print(f"Added item {text_edit}")
+        ui.listWidget.addItem(text_edit)
+
+
+def on_remove_button_clicked():
+    indexes = ui.listWidget.selectedItems()
+
+    num_indexes = len(indexes)
+
+    if num_indexes > 1:
+        for i, k in enumerate(indexes):
+            index = ui.listWidget.indexFromItem(k).row()
+            ui.listWidget.takeItem(index)
+    elif num_indexes == 1:
+        y = 0
+        for i, k in enumerate(indexes):
+            index = ui.listWidget.indexFromItem(k).row()
+            if index > y:
+                y = index
+
+        ui.listWidget.takeItem(y)
+    else:
+        ui.listWidget.takeItem(ui.listWidget.count()-1)
 
 
 def on_close():
@@ -22,6 +56,6 @@ def on_close():
 
 ui.actionQuit.triggered.connect(on_close)
 ui.item_add_button.clicked.connect(on_add_button_clicked)
-
+ui.item_remove_button.clicked.connect(on_remove_button_clicked)
 
 sys.exit(app.exec())
