d9e013afb8b1045201ebe0cd6e849c3d4a08387c
CAI_4 <sunsetcity93@gmail.com>
2024-05-31 01:32:02 -0700

    Updated parser_mki.ino - Added sensor board configuration - Added state machine

--- arduino/parser_mki/parser_mki.ino
+++ arduino/parser_mki/parser_mki.ino
@@ -1,4 +1,7 @@
-// Debug 
+#include <RGBLed.h>         // RGB LED lib for status LED
+#include <HCSR04.h>         // Ultrasonic sensor library
+
+/// Debug 
 #define debug_flag 0
 
 #if debug_flag
@@ -11,16 +14,38 @@
    #define debugln(x)
 #endif
 
+// Ultrasonic Module 
+const uint8_t trigger_pin = 3;
+const uint8_t echo_pin = 4;
+
+double distance;                              // Represents the current measured distance by the sensor
+double clearance_distance = 10.0;             // Set minimum working distance to be 10cm
+
 // Motor Driver
-const int m1_pulse_pin = 8;
-const int m1_direction_pin = 9;
-const int m1_enable_pin = 10;
+const uint8_t l_dir_pin = 5;
+const uint8_t l_en_pin = 6;
+const uint8_t l_pul_pin = 7;
 
-const int m2_pulse_pin = 11;
-const int m2_direction_pin = 12;
-const int m2_enable_pin = 13;
+const uint8_t r_dir_pin = 8;
+const uint8_t r_en_pin = 9;
+const uint8_t r_pul_pin = 10;
 
-const int DV_MUX = 500;
+/// Sensor Board
+// Status RGB LED
+const uint8_t R_pin = 12;
+const uint8_t G_pin = 11;
+const uint8_t B_pin = 13;
+
+RGBLed status_led(R_pin, G_pin, B_pin, RGBLed::COMMON_ANODE);
+
+// CNY70 Sensors
+const uint8_t S0_pin = A0;    // Right Sensor
+const uint8_t S1_pin = A1;    // Middle Sensor
+const uint8_t S2_pin = A2;    // Left Sensor
+
+const bool S0 = false;
+const bool S1 = false;
+const bool S2 = false;
 
 // Serial Directives
 const String wait_directive = "WAIT";
@@ -48,19 +73,30 @@ int drive_speed = NOM;
 // Simple State Machine
 const int IDLE = 0;
 const int DRIVE = 1;
+const int STUCK = 2;
+const int ERROR = 3;
 int state = IDLE;
+int prev_state = state;
 
 void setup() {
   Serial.begin(115200);
   while(!Serial) {}
 
-  pinMode(m1_direction_pin, OUTPUT);
-  pinMode(m1_enable_pin, OUTPUT);
-  pinMode(m1_pulse_pin, OUTPUT);
+  HCSR04.begin(trigger_pin, echo_pin);
 
-  pinMode(m2_direction_pin, OUTPUT);
-  pinMode(m2_enable_pin, OUTPUT);
-  pinMode(m2_pulse_pin, OUTPUT);
+  pinMode(l_dir_pin, OUTPUT);
+  pinMode(l_en_pin , OUTPUT);
+  pinMode(l_pul_pin, OUTPUT);
+
+  pinMode(r_dir_pin, OUTPUT);
+  pinMode(r_en_pin , OUTPUT);
+  pinMode(r_pul_pin, OUTPUT);
+
+  pinMode(S0_pin, INPUT);
+  pinMode(S1_pin, INPUT);
+  pinMode(S2_pin, INPUT);
+
+  measure_clearance();
 }
 
 void loop() {
@@ -134,47 +170,104 @@ void loop() {
           drive_speed = SLOW;
         }
       }
+      // else
+      // {
+      //   state = ERROR;
+      // }
     }
 
+    // Since RPi5 request a reply, send the processed message.
     Serial.println("Processed message: " + msg);
+    
+    // Delay allows the serial buffer to be adequately filled
     delay(100);
+  }
+
+  // Update distance before any state desicion
+  measure_clearance();
+
+  if (distance < clearance_distance) 
+  {
+    prev_state = state;
+    state = STUCK;
   }
 
   switch (state) 
   {
     case DRIVE:
+      // Drive to whatever target speed
       drive_state();
       break;
+    
     case IDLE:
-      digitalWrite(m1_enable_pin, LOW);
-      digitalWrite(m1_pulse_pin, LOW);
+      // Turn OFF the Motors
+      status_led.setColor(RGBLed::BLUE);
+      drive_speed = STOP;
+      break;
+    
+    case STUCK:
+      // Stop any movement and set the status LED to Orange
+      // Wait for 10 seconds before checking if unstuck
+      status_led.setColor(255, 165, 0);
 
-      digitalWrite(m2_enable_pin, LOW);
-      digitalWrite(m2_pulse_pin, LOW);
+      drive_speed = STOP;
+
+      while (distance < clearance_distance)
+      {
+        delay(5000);
+        measure_clearance();              // Update measured distance
+      }
+
+      state = prev_state;
+      break;
+
+    case ERROR:
+      // Turn OFF the Motors and set status LED to blinking red
+      status_led.flash(RGBLed::RED, 250);
       break;
   }
 }
 
 void drive_state()
 {
-  // digitalWrite(LED, HIGH);
-  // delay(100);
-  // digitalWrite(LED, LOW);
+  // Ask CNY70 sensors for their state and drive accordingly.
+  update_sensors();
 
-  for (int i = 0; i < drive_speed; i++)
+  switch (direction)
   {
-    digitalWrite(m1_direction_pin, direction);
-    digitalWrite(m1_enable_pin, HIGH);
-    digitalWrite(m1_pulse_pin, HIGH);
+    case FORWARD:
+      for (int i = 0; i < drive_speed; i++)
+      {
+        digitalWrite(l_dir_pin, LOW);
+        digitalWrite(r_dir_pin, LOW);
+        digitalWrite(l_en_pin, HIGH);
+        digitalWrite(r_en_pin, HIGH);
+        digitalWrite(l_pul_pin, HIGH);
+        digitalWrite(r_pul_pin, HIGH);
+        delayMicroseconds(250);
+        
+        digitalWrite(l_pul_pin, LOW);
+        digitalWrite(r_pul_pin, LOW);
+        delayMicroseconds(250);
+      }
+      break;
 
-    digitalWrite(m2_direction_pin, direction);
-    digitalWrite(m2_enable_pin, HIGH);
-    digitalWrite(m2_pulse_pin, HIGH);
-    delayMicroseconds(DV_MUX);
-
-    digitalWrite(m1_pulse_pin, LOW);
-    digitalWrite(m2_pulse_pin, LOW);
-    delayMicroseconds(DV_MUX);
+    case REVERSE:
+      for (int i = 0; i < drive_speed; i++)
+      {
+        digitalWrite(l_dir_pin, HIGH);
+        digitalWrite(r_dir_pin, HIGH);
+        digitalWrite(l_en_pin, HIGH);
+        digitalWrite(r_en_pin, HIGH);
+        digitalWrite(l_pul_pin, HIGH);
+        digitalWrite(r_pul_pin, HIGH);
+        delayMicroseconds(250);
+        
+        digitalWrite(l_pul_pin, LOW);
+        digitalWrite(r_pul_pin, LOW);
+        delayMicroseconds(250);
+      }
+      break;
   }
 }
 
@@ -182,10 +275,52 @@ void rotate(int dir)
 {
     if (dir == RIGHT)
     {
-      
+      for (int i = 0; i < drive_speed; i++)
+      {
+        digitalWrite(l_dir_pin, LOW);
+        digitalWrite(r_dir_pin, HIGH);
+        digitalWrite(l_en_pin, HIGH);
+        digitalWrite(r_en_pin, HIGH);
+        digitalWrite(l_pul_pin, HIGH);
+        digitalWrite(r_pul_pin, HIGH);
+        delayMicroseconds(250);
+        
+        digitalWrite(l_pul_pin, LOW);
+        digitalWrite(r_pul_pin, LOW);
+        delayMicroseconds(250);
+      }
     }
     else if (dir == LEFT)
     {
-      
+      for (int i = 0; i < drive_speed; i++)
+      {
+        digitalWrite(l_dir_pin, HIGH);
+        digitalWrite(r_dir_pin, LOW);
+        digitalWrite(l_en_pin, HIGH);
+        digitalWrite(r_en_pin, HIGH);
+        digitalWrite(l_pul_pin, HIGH);
+        digitalWrite(r_pul_pin, HIGH);
+        delayMicroseconds(250);
+        
+        digitalWrite(l_pul_pin, LOW);
+        digitalWrite(r_pul_pin, LOW);
+        delayMicroseconds(250);
+      }
     }
+}
+
+void measure_clearance()
+{
+  double* ds = HCSR04.measureDistanceCm();
+  distance = ds[0];
+
+  debug("1: ");
+  debug(distance);
+  debugln(" cm");
+  debugln("---");
+}
+
+void update_sensors()
+{
+
 }
